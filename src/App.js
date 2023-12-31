c62ee37ede02c7592f54747aa23f56012b238160
HipsterSantos <santoscampos269@gmail.com>
2023-12-31 15:03:50 +0100

    [update] update framer motion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
-function App() {
+const variants = {
+  enter: {
+    opacity: 0,
+  },
+  center: {
+    opacity: 1,
+  },
+  exit: {
+    opacity: 0,
+  },
+};
+
+const App = () => {
+  const [showComponentB, setShowComponentB] = useState(false);
+
+  const toggleComponent = () => {
+    setShowComponentB(!showComponentB);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={toggleComponent}>Toggle Component</button>
+
+      <AnimatePresence exitBeforeEnter>
+        {showComponentB ? (
+          <motion.div
+            key="componentB"
+            initial="enter"
+            animate="center"
+            exit="exit"
+            variants={variants}
+          >
+            <ComponentB />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="componentA"
+            initial="enter"
+            animate="center"
+            exit="exit"
+            variants={variants}
+          >
+            <ComponentA />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
-}
+};
+
+const ComponentA = () => {
+  return (
+    <div>
+      <h2>Component A</h2>
+      <p>This is Component A.</p>
+    </div>
+  );
+};
+
+const ComponentB = () => {
+  return (
+    <div>
+      <h2>Component B</h2>
+      <p>This is Component B.</p>
+    </div>
+  );
+};
 
 export default App;
+
